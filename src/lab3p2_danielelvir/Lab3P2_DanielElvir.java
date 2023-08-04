a0efd2eee86e7b5fc4573bc9ba9e622e06cf31f2
Daniel Elvir <danielisrael.elvir@gmail.com>
2023-08-04 14:02:34 -0600

    Creación de clase padre e hijas

--- src/lab3p2_danielelvir/Lab3P2_DanielElvir.java
+++ src/lab3p2_danielelvir/Lab3P2_DanielElvir.java
@@ -14,7 +14,10 @@ public class Lab3P2_DanielElvir {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        boolean seguir=true;
+        while (seguir) {            
+            
+        }
     }
     
 }
